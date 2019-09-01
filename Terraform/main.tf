a62b3098f19e7eaaf8957772697ff67d476589a5
Ubuntu <ubuntu@ip-172-31-11-75.eu-west-1.compute.internal>
2019-09-01 10:55:32 +0000

    db probes

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,36 +9,50 @@ provider "aws" {
     instance_type = "t2.micro"
     key_name      = "cloud-operations"
     user_data      = "${file("./script.sh")}" 
-    subnet_id     = "${aws_subnet.public-subnet.id}"
-    vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
+#    subnet_id     = "${aws_subnet.public-subnet.id}"
+#    vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
     associate_public_ip_address = true
     tags = {
       Name = "nginx-container"
     }
     
     provisioner "remote-exec" {
-      environment_vars = {
-        "HOST_NAME" = data.aws_instance.nginx-container.public_dns
+      inline = [
+        "export HOST_NAME=${aws_instance.nginx-container.public_dns}"
+      ]
+      
+      connection {
+        type = "ssh"
+        host = "${aws_instance.nginx-container.public_ip}"
+        user = "ubuntu"
+        private_key = "${file("./cloud-operations.pem")}"
       }
     }
     
   }
 
- resource "aws_db_instance" "default" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7.22"
-  instance_class       = "db.t2.micro"
-  name                 = "mydb"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot = true
-  vpc_security_group_ids = ["${aws_security_group.sgdb.id}"]
-  db_subnet_group_name   = "${aws_db_subnet_group.subnet_group.id}"
-  availability_zone = "eu-west-1b"
-}
+ resource "aws_instance" "database" {
+   ami = "ami-06358f49b5839867c"
+   instance_type = "t2.micro"
+   key_name = "cloud-operations"
+   associate_public_ip_address = true
+   tags = {
+     name = "database-instance"
+   }
+ }
+#  storage_type         = "gp2"
+#  engine               = "mysql"
+#  engine_version       = "5.7.22"
+#  instance_class       = "db.t2.micro"
+#  name                 = "mydb"
+#  username             = "foo"
+#  password             = "foobarbaz"
+#  parameter_group_name = "default.mysql5.7"
+#  skip_final_snapshot = true
+#  vpc_security_group_ids = ["${aws_security_group.sgdb.id}"]
+#  db_subnet_group_name   = "${aws_db_subnet_group.subnet_group.id}"
+#  availability_zone = "eu-west-1b"
+#}
 
 
 
