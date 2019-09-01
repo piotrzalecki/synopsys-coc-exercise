5495a2f672005bc1d4ffd2c543b38b264c848b6b
Ubuntu <ubuntu@ip-172-31-11-75.eu-west-1.compute.internal>
2019-09-01 00:05:10 +0000

    Committer: Ubuntu <ubuntu@ip-172-31-11-75.eu-west-1.compute.internal>  On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   Ansible/database.yml 	new file:   Terraform/.gitignore 	modified:   Terraform/main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,13 +8,20 @@ provider "aws" {
     ami           = "ami-06358f49b5839867c"
     instance_type = "t2.micro"
     key_name      = "cloud-operations"
-    userdata      = "${file("./script.sh")}" 
+    user_data      = "${file("./script.sh")}" 
     subnet_id     = "${aws_subnet.public-subnet.id}"
     vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
     associate_public_ip_address = true
     tags = {
       Name = "nginx-container"
     }
+    
+    provisioner "remote-exec" {
+      environment_vars = {
+        "HOST_NAME" = data.aws_instance.nginx-container.public_dns
+      }
+    }
+    
   }
 
  resource "aws_db_instance" "default" {
