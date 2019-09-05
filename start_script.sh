a95345f711b1723daefaf695772c046f338ad376
Ubuntu <ubuntu@ip-172-31-11-75.eu-west-1.compute.internal>
2019-09-05 09:46:52 +0000

    docker close to work

---
+++ start_script.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+#Setting environement variables
+export DB_NAME=testdb
+export DB_USERNAME=appuser
+export DB_PASSWORD=somepassword
+
+echo "--- Environment variables has been exported ---"
+
